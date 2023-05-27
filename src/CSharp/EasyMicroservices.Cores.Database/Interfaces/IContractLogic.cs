96b892d2cdca61f9d1dbd61aeef8f4ea7438f936
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-27 18:17:06 +0330

    Support for IContract

---
+++ src/CSharp/EasyMicroservices.Cores.Database/Interfaces/IContractLogic.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyMicroservices.Cores.Database.Interfaces
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TRequestContract"></typeparam>
+    /// <typeparam name="TResponseContract"></typeparam>
+    /// <typeparam name="TId"></typeparam>
+    public interface IContractLogic<TEntity, TRequestContract, TResponseContract, TId> : IContractReadableLogic<TEntity, TResponseContract, TId>, IContractWritableLogic<TEntity, TRequestContract, TId>
+    {
+
+    }
+}
--- src/CSharp/EasyMicroservices.Cores.Database/Interfaces/IWritableLogic.cs
+++ src/CSharp/EasyMicroservices.Cores.Database/Interfaces/IWritableLogic.cs
@@ -17,4 +17,21 @@ namespace EasyMicroservices.Cores.Database.Interfaces
         /// <returns></returns>
         Task<MessageContract<TEntity>> Update(TEntity entity, CancellationToken cancellationToken = default);
     }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TContract"></typeparam>
+    /// <typeparam name="TId"></typeparam>
+    public interface IContractWritableLogic<TEntity, TContract, TId>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contract"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<MessageContract<TId>> Add(TContract contract, CancellationToken cancellationToken = default);
+    }
 }
