00f3fe91ff0342fa4a862d3f9f6196aadd2d44fd
Ali Yousefi <ali.visual.studio@gmail.com>
2023-06-07 13:04:21 +0330

    fix HttpPost and save data

--- src/CSharp/EasyMicroservices.Cores.AspCoreApi/SimpleQueryServiceController.cs
+++ src/CSharp/EasyMicroservices.Cores.AspCoreApi/SimpleQueryServiceController.cs
@@ -17,34 +17,40 @@ namespace EasyMicroservices.Cores.AspCoreApi
     [Route("[controller]")]
     public class SimpleQueryServiceController<TEntity, TId, TRequestContract, TResponseContract> : ControllerBase
     {
-        private readonly IContractLogic<TEntity, TRequestContract, TResponseContract, TId> _contractReadable;
+        private readonly IContractLogic<TEntity, TRequestContract, TResponseContract, TId> _contractLogic;
         /// <summary>
         /// 
         /// </summary>
         /// <param name="contractReadable"></param>
         public SimpleQueryServiceController(IContractLogic<TEntity, TRequestContract, TResponseContract, TId> contractReadable)
         {
-            _contractReadable = contractReadable;
+            _contractLogic = contractReadable;
         }
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="id"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
+        [HttpGet]
         public Task<MessageContract<TResponseContract>> GetById(TId id, CancellationToken cancellationToken = default)
         {
-            return _contractReadable.GetById(id, cancellationToken);
+            return _contractLogic.GetById(id, cancellationToken);
         }
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="request"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        public Task<MessageContract<TId>> Add(TRequestContract request, CancellationToken cancellationToken = default)
+        [HttpPost]
+        public async Task<MessageContract<TId>> Add(TRequestContract request, CancellationToken cancellationToken = default)
         {
-            return _contractReadable.Add(request, cancellationToken);
+            var result = await  _contractLogic.Add(request, cancellationToken);
+            await _contractLogic.SaveChangesAsync(cancellationToken);
+            return result;
         }
     }
 }
