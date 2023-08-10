e2e76ea931cf663c2c3400cfb8d7af9e357538be
Ali Yousefi <ali.visual.studio@gmail.com>
2023-08-10 12:17:29 +0330

    Support and indexes for interfaces in core context

---
+++ src/CSharp/EasyMicroservices.Cores.Relational.EntityFrameworkCore/RelationalCoreContext.cs
@@ -0,0 +1,31 @@
+﻿using EasyMicroservices.Cores.EntityFrameworkCore;
+using EasyMicroservices.Cores.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyMicroservices.Cores.Sql.EntityFrameworkCore
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RelationalCoreContext : CoreContext
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var entityType in Model.GetEntityTypes())
+            {
+                if (typeof(IUniqueIdentitySchema).IsAssignableFrom(entityType.ClrType))
+                {
+                    modelBuilder.Entity(entityType.ClrType)
+                        .Property(nameof(IUniqueIdentitySchema.UniqueIdentity))
+                        .UseCollation("SQL_Latin1_General_CP1_CS_AS");
+                }
+            }
+        }
+    }
+}
