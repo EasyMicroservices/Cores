4cba462c15f81c81772c8de33f0bf680225e5ed8
Ali Yousefi <ali.visual.studio@gmail.com>
2023-08-10 12:17:47 +0330

    Add tests for interfaces in core context

--- src/CSharp/EasyMicroservices.Cores.Tests/DatabaseLogics/Database/Entities/UserEntity.cs
+++ src/CSharp/EasyMicroservices.Cores.Tests/DatabaseLogics/Database/Entities/UserEntity.cs
@@ -2,10 +2,14 @@
 
 namespace EasyMicroservices.Cores.Tests.DatabaseLogics.Database.Entities
 {
-    public class UserEntity : IIdSchema<long>, IUniqueIdentitySchema
+    public class UserEntity : IIdSchema<long>, IUniqueIdentitySchema, IDateTimeSchema, ISoftDeleteSchema
     {
         public long Id { get; set; }
         public string UserName { get; set; }
         public string UniqueIdentity { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedDateTime { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public DateTime? ModificationDateTime { get; set; }
     }
 }
