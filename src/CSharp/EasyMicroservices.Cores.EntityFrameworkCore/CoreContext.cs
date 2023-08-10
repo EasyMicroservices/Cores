3ae251f0ebd119462d792612f13959da50f160cf
Ali Yousefi <ali.visual.studio@gmail.com>
2023-08-10 12:17:23 +0330

    Support and indexes for interfaces in core context

--- src/CSharp/EasyMicroservices.Cores.EntityFrameworkCore/CoreContext.cs
+++ src/CSharp/EasyMicroservices.Cores.EntityFrameworkCore/CoreContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EasyMicroservices.Cores.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
 
 namespace EasyMicroservices.Cores.EntityFrameworkCore
 {
@@ -7,6 +10,44 @@ namespace EasyMicroservices.Cores.EntityFrameworkCore
     /// </summary>
     public class CoreContext : DbContext
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            foreach (var entityType in Model.GetEntityTypes())
+            {
+                if (typeof(IUniqueIdentitySchema).IsAssignableFrom(entityType.ClrType))
+                {
+                    modelBuilder.Entity(entityType.ClrType).HasIndex(nameof(IUniqueIdentitySchema.UniqueIdentity));
+                }
+
+                if (typeof(IDateTimeSchema).IsAssignableFrom(entityType.ClrType))
+                {
+                    modelBuilder.Entity(entityType.ClrType).HasIndex(nameof(IDateTimeSchema.CreationDateTime));
+                    modelBuilder.Entity(entityType.ClrType).HasIndex(nameof(IDateTimeSchema.ModificationDateTime));
+                }
+
+                if (typeof(ISoftDeleteSchema).IsAssignableFrom(entityType.ClrType))
+                {
+                    modelBuilder.Entity(entityType.ClrType).HasIndex(nameof(ISoftDeleteSchema.IsDeleted));
+                    modelBuilder.Entity(entityType.ClrType).HasIndex(nameof(ISoftDeleteSchema.DeletedDateTime));
+                }
+
+                foreach (var property in entityType.ClrType.GetProperties())
+                {
+                    if (property.PropertyType == typeof(DateTime))
+                    {
+                        DateTimeKind dateTimeKind = DateTimeKind.Local;
+                        var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+                           v => v, v => DateTime.SpecifyKind(v, dateTimeKind));
+                        modelBuilder.Entity(entityType.ClrType).Property(property.Name).HasConversion(dateTimeConverter);
+                    }
+                }
+            }
+        }
     }
 }
