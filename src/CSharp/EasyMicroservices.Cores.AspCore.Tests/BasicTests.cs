e624f0d44432cccfb50b96a79b6ad1c87438e579
Ali Yousefi <ali.visual.studio@gmail.com>
2023-08-22 14:56:43 +0330

    add asp core tests

---
+++ src/CSharp/EasyMicroservices.Cores.AspCore.Tests/BasicTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.TestHost;
+namespace EasyMicroservices.Cores.AspCore.Tests
+{
+    public class BasicTests
+    {
+        protected TestServer _testServer;
+        public BasicTests()
+        {
+            var webBuilder = new WebHostBuilder();
+            webBuilder.UseStartup<Startup>();
+
+            _testServer = new TestServer(webBuilder);
+        }
+
+        [Theory]
+        [InlineData("/")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            var client = _testServer.CreateClient();
+            var data = await client.GetStringAsync($"api/user/getall");
+        }
+    }
+}
---
+++ src/CSharp/EasyMicroservices.Cores.AspCore.Tests/Program.cs
@@ -0,0 +1,32 @@
+﻿using EasyMicroservices.Cores.AspEntityFrameworkCoreApi;
+using EasyMicroservices.Cores.Relational.EntityFrameworkCore.Intrerfaces;
+using EasyMicroservices.Cores.Tests.DatabaseLogics.Database.Contexts;
+using EasyMicroservices.Cores.Tests.DatabaseLogics.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyMicroservices.Cores.AspCore.Tests
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var app = StartUpExtensions.Create<MyTestContext>(args);
+            app.Services.Builder<MyTestContext>();
+            app.Services.AddScoped((serviceProvider) => new UnitOfWork(serviceProvider).GetContractLogic<UserEntity, UserEntity, UserEntity, UserEntity>());
+            app.Services.AddTransient(serviceProvider => new MyTestContext(serviceProvider.GetService<IEntityFrameworkCoreDatabaseBuilder>()));
+            app.Services.AddScoped<IEntityFrameworkCoreDatabaseBuilder>(serviceProvider => new DatabaseBuilder());
+
+            var build = app.Build<MyTestContext>();
+            build.MapControllers();
+            build.Run();
+        }
+    }
+
+    public class DatabaseBuilder : IEntityFrameworkCoreDatabaseBuilder
+    {
+        public void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Test DB");
+        }
+    }
+}
