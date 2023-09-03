b8888edf72a1d8d847242b482967c58377458f6a
Ali Yousefi <ali.visual.studio@gmail.com>
2023-09-03 18:36:45 +0330

    Add support for base entities

---
+++ src/CSharp/EasyMicroservices.Cores.Database/Database/Entities/DateTimeEntity.cs
@@ -0,0 +1,20 @@
+﻿using EasyMicroservices.Cores.Interfaces;
+using System;
+
+namespace EasyMicroservices.Cores.Database.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DateTimeEntity : IDateTimeSchema
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime CreationDateTime { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? ModificationDateTime { get; set; }
+    }
+}
---
+++ src/CSharp/EasyMicroservices.Cores.Database/Database/Entities/FullAbilityEntity.cs
@@ -0,0 +1,34 @@
+﻿using EasyMicroservices.Cores.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyMicroservices.Cores.Database.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FullAbilityEntity : IUniqueIdentitySchema, ISoftDeleteSchema, IDateTimeSchema
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime CreationDateTime { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? ModificationDateTime { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsDeleted { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? DeletedDateTime { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string UniqueIdentity { get; set; }
+    }
+}
