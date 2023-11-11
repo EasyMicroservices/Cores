efd2ee169fc24f192832b8425a380289f3fbc6dd
Ali Yousefi <ali.visual.studio@gmail.com>
2023-11-11 22:52:53 +0330

    fix endpoints and permissions

--- src/CSharp/EasyMicroservices.Cores.AspCoreApi/Authorizations/AspCoreAuthorization.cs
+++ src/CSharp/EasyMicroservices.Cores.AspCoreApi/Authorizations/AspCoreAuthorization.cs
@@ -46,8 +46,8 @@ namespace EasyMicroservices.Cores.AspCoreApi.Authorizations
         /// <exception cref="NotImplementedException"></exception>
         public async Task<MessageContract> CheckIsAuthorized(HttpContext httpContext)
         {
-            var hasPermission = await HasPermission(httpContext).AsCheckedResult();
-            if (!hasPermission)
+            var hasPermission = await HasPermission(httpContext);
+            if (!hasPermission.IsSuccess)
                 return (FailedReasonType.AccessDenied, "Sorry, you cannot call this service, you have not enough permissions!");
             return true;
         }
@@ -81,6 +81,9 @@ namespace EasyMicroservices.Cores.AspCoreApi.Authorizations
 
         async Task<MessageContract<bool>> HasPermission(HttpContext httpContext)
         {
+            var endpoints = httpContext.GetEndpoint();
+            if (endpoints == null)
+                return true;
             var controllerActionDescriptor = httpContext.GetEndpoint().Metadata.GetMetadata<ControllerActionDescriptor>();
             if (controllerActionDescriptor.ControllerTypeInfo.GetCustomAttributes(typeof(AllowAnonymousAttribute)).Any() ||
                 controllerActionDescriptor.MethodInfo.GetCustomAttributes(typeof(AllowAnonymousAttribute)).Any())
