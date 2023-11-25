08941d4bdf1305ce285b0c33945c753b11b32b7e
Ali Yousefi <ali.visual.studio@gmail.com>
2023-11-25 18:26:57 +0330

    Add support for EasyMicroservices.Cores.Clients

---
+++ src/CSharp/EasyMicroservices.Cores.Clients/CoreContractResolver.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+
+namespace EasyMicroservices.Cores.Clients
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CoreContractResolver : DefaultContractResolver
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="member"></param>
+        /// <param name="memberSerialization"></param>
+        /// <returns></returns>
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var jsonProp = base.CreateProperty(member, memberSerialization);
+            jsonProp.Required = Required.Default;
+            return jsonProp;
+        }
+    }
+}
