29853e5fe7779d4bc7b82fa3a06710ffd01525d8
Ali Yousefi <ali.visual.studio@gmail.com>
2024-02-13 21:51:27 +0330

    Add support for EasyMicroservices.Cores.Infrastructure

---
+++ src/CSharp/EasyMicroservices.Cores.Infrastructure/Interfaces/IWidget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.Cores.Interfaces;
+public interface IWidget
+{
+    void Build();
+    Type GetObjectType();
+}
+
+public interface IWidget<T>: IWidget
+{
+    Task Initialize(params T[] parameters);
+}
---
+++ src/CSharp/EasyMicroservices.Cores.Infrastructure/Widgets/WidgetManager.cs
@@ -0,0 +1,44 @@
+﻿using EasyMicroservices.Cores.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EasyMicroservices.Cores.Widgets;
+public class WidgetManager : IWidgetManager
+{
+    readonly ConcurrentDictionary<Type, List<IWidget>> Widgets = new ConcurrentDictionary<Type, List<IWidget>>();
+
+    public void Register(IWidget widget)
+    {
+        var type = widget.GetObjectType();
+        if (Widgets.TryGetValue(type, out List<IWidget> widgets))
+            widgets.Add(widget);
+        else
+        {
+            Widgets.TryAdd(widget.GetObjectType(), new List<IWidget>()
+            {
+                widget
+            });
+        }
+    }
+
+    public void UnRegister(IWidget widget)
+    {
+        Widgets.TryRemove(widget.GetObjectType(), out _);
+    }
+
+    public IEnumerable<IWidget> GetWidgetsByType(Type type)
+    {
+        if (Widgets.TryGetValue(type, out List<IWidget> widgets))
+            return widgets.ToList();
+        return Enumerable.Empty<IWidget>();
+    }
+
+    public IEnumerable<T> GetWidgets<T>() where T : IWidget
+    {
+        if (Widgets.TryGetValue(typeof(T), out List<IWidget> widgets))
+            return widgets.Cast<T>().ToList();
+        return Enumerable.Empty<T>();
+    }
+}
